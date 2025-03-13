9643230634fc544299eff7fb45d3b53654a9ab52
Han <jumdo12@gmail.com>
2025-03-13 11:09:55 +0900

    test(PlayerTest): 불필요한 테스트 제거

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -14,22 +14,6 @@ import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
 
-
-    @Test
-    void 플레이어를_생성한다() {
-        //given
-        final String name = "윌슨";
-        final List<Card> cards = List.of(new Card(DIAMOND, ACE), new Card(DIAMOND, EIGHT));
-        final CardGroup cardGroup = new CardGroup(cards);
-
-        //when
-        final Player player = new Player(name, cardGroup);
-
-        //then
-        assertThat(player).isInstanceOf(Player.class);
-
-    }
-
     @Test
     void 플레이어에게_카드를_준다() {
         //given
